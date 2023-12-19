510d565cfddc122b0d0501e2b9a13bd99f6a0f86
Bobo100 <w150lione@gmail.com>
2023-12-19 11:43:54 +0800

    [what] missing cookie setting

--- api/serverless.ts
+++ api/serverless.ts
@@ -5,11 +5,18 @@ dotenv.config();
 // Require the framework
 import Fastify from "fastify";
 import { FastifyRequest, FastifyReply } from "fastify";
+import cookie from "@fastify/cookie";
+import { FastifyCookieOptions } from "@fastify/cookie";
 
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
+
+app.register(cookie, {
+  secret: "my-secret", // for cookies signature
+  parseOptions: {}, // options for parsing cookies
+} as FastifyCookieOptions);
 
 // Register your application as a normal plugin.
 app.register(import("../src/index"), {
