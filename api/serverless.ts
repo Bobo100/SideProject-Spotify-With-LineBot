3492daf22555e53935763f143fca817604aa700c
Bobo100 <w150lione@gmail.com>
2023-12-19 11:04:05 +0800

    [what] 更新成範本 (測試用)

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,31 +1,14 @@
-"use strict";
-
 import fastify, {
   FastifyServerOptions,
   FastifyRequest,
   FastifyReply,
 } from "fastify";
-import cookie from "@fastify/cookie";
-import { FastifyCookieOptions } from "@fastify/cookie";
-import spotify from "../functions/spotfiy";
-import spotifyCallback from "../functions/spotify-callback";
 
 const fastifyOptions: FastifyServerOptions = {
   logger: true,
-  // disableRequestLogging: false,
 };
 
 const server = fastify(fastifyOptions);
-
-// https://github.com/fastify/fastify-cookie
-server.register(cookie, {
-  secret: "my-secret", // for cookies signature
-  parseOptions: {}, // options for parsing cookies
-} as FastifyCookieOptions);
-
-// server.register(spotify, { prefix: "/" });
-
-// server.register(spotifyCallback, { prefix: "/api/spotify-callback" });
 
 server.register(import("../functions/index"), {
   prefix: "/",
@@ -35,11 +18,3 @@ export default async (req: FastifyRequest, res: FastifyReply) => {
   await server.ready();
   server.server.emit("request", req, res);
 };
-
-// server.listen({ port: 3000 }, (err, address) => {
-//   if (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-//   console.log(`Server listening at ${address}`);
-// });
